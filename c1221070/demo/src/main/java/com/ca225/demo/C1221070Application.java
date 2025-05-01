18a9b36018fd8fcbf485ed215bf4ebdfc0eab5fe
ENG ABDIKANI BASHIIR <abdiqani2259@gmail.com>
2025-05-01 22:54:59 +0300

    c1221070 -abdikani bashir hussen

---
+++ c1221070/demo/src/main/java/com/ca225/demo/C1221070Application.java
@@ -0,0 +1,13 @@
+package com.ca225.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1221070Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1221070Application.class, args);
+	}
+
+}
---
+++ c1221070/demo/src/test/java/com/ca225/demo/C1221070ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca225.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1221070ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
